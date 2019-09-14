f9591d5f4340fc06dd80fb3e2155dce03ee962d0
its-raju <itsrajuios@gmail.com>
2019-09-14 12:58:32 +0530

    initial Setup

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Admin on 14/09/19.
+//  Copyright © 2019 TryCatch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
